21e0fe82eaacc04f826c6bb214f94e330d2b7d8e
Tomohiro Takatsuka <tomohiro.takatsuka@springernature.com>
2022-12-18 18:01:01 +0900

    add resource

--- GCP/Run/main.tf
+++ GCP/Run/main.tf
@@ -1,1 +1,20 @@
 # lecture 45-48:
+
+# google_cloud_run_service: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
+
+# See link below for argument reference:
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
+
+# lecture 45:
+resource "google_cloud_run_service" "run-app-from-tf" {
+    name = "run-app-from-tf"
+    location = "asia-southeast1"
+    template {
+      spec {
+        containers {
+# https://console.cloud.google.com/gcr/images/google-samples/global/hello-app
+            image = "gcr.io/google-samples/hello-app:1.0"
+        }
+      }
+    }
+}
