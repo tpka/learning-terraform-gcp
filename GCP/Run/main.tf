2243a8d964ded81dc4ae05dbf945c37feac5b626
Tomohiro Takatsuka <tomohiro.takatsuka@springernature.com>
2022-12-19 22:47:24 +0900

    add resource blocks

--- GCP/Run/main.tf
+++ GCP/Run/main.tf
@@ -1,6 +1,8 @@
 # lecture 45-48:
 
 # google_cloud_run_service: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
+# IAM policy for Cloud Run Service: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service_iam#google_cloud_run_service_iam_policy
+
 
 # See link below for argument reference:
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
@@ -17,4 +19,19 @@ resource "google_cloud_run_service" "run-app-from-tf" {
         }
       }
     }
+}
+
+# lecture 46:
+# allow public access
+resource "google_cloud_run_service_iam_policy" "pub_access" {
+  service = google_cloud_run_service.run-app-from-tf.name
+  location = google_cloud_run_service.run-app-from-tf.location
+  policy_data = data.google_iam_policy.pub-1.policy_data
+}
+
+data "google_iam_policy" "pub-1" {
+  binding {
+    role = "roles/run.invoker"
+    members = [ "allUsers" ]
+  }
 }
