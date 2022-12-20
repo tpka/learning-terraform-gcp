bdba0011b00b463cfefa7627780b3726047bd9ab
Tomohiro Takatsuka <tomohiro.takatsuka@springernature.com>
2022-12-20 16:33:22 +0900

    add links

--- GCP/Run/main.tf
+++ GCP/Run/main.tf
@@ -2,7 +2,7 @@
 
 # google_cloud_run_service: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
 # IAM policy for Cloud Run Service: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service_iam#google_cloud_run_service_iam_policy
-
+# google_iam_policy: https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/iam_policy
 
 # See link below for argument reference:
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
@@ -23,12 +23,16 @@ resource "google_cloud_run_service" "run-app-from-tf" {
 
 # lecture 46:
 # allow public access
+# google_cloud_run_service_iam_policy: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service_iam#google_cloud_run_service_iam_policy
 resource "google_cloud_run_service_iam_policy" "pub_access" {
   service = google_cloud_run_service.run-app-from-tf.name
   location = google_cloud_run_service.run-app-from-tf.location
   policy_data = data.google_iam_policy.pub-1.policy_data
 }
 
+# lecture 46:
+# attaching role
+# google_iam_policy: https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/iam_policy
 data "google_iam_policy" "pub-1" {
   binding {
     role = "roles/run.invoker"
